d140659ce3d9bfca0474907f171b665d875275ae
MATT-07 <117632549+MATT-07@users.noreply.github.com>
2023-01-23 09:18:07 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import pyxel
+
+pyxel.init(160, 120)
+
+def deplacement(x,y):
+  if pyxel.btn(pyxel.KEY_RIGHT):
+        if x < 120:
+            x = x + 2
+  if pyxel.btn(pyxel.KEY_LEFT):
+    if x > 0:
+      x = x - 2
+  if pyxel.btn(pyxel.KEY_UP):
+      y = y+1
+    
+    return x,y
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(10, 10, 20, 20, 11)
+
+pyxel.run(update, draw)
