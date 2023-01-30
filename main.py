d34484ad23f39ebd812ff96f75644ef0546dbea2
Vincent <81516479+vincent-fantino@users.noreply.github.com>
2023-01-30 09:00:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ l = 960 #dimensions de la fenêtre
 ymax = 216
 ymin = 152 #coordonées maximales et minimales de déplacement
 x,y = 22,22 #coordonnées du personnage
-lv1 = True
-lv2 = False #variables pour savoir dans quel niveau on se trouve si = True, niveau actuel
+niveau = 1 #variable pour savoir dans quel niveau on se trouve
 
 def deplacement(x,y):
     if pyxel.btn(pyxel.KEY_RIGHT):
@@ -29,13 +28,13 @@ def update():
     global x,y,ymin,ymax,lv1,lv2
     x,y = deplacement(x,y) #fonction de mise à jour des coordonnées du personnage en fonction des touches appuyées, à l'aide de la fonction deplacement
 
-def draw_lv1():
+def draw():
     global x,y,lv1,lv2,ymin,ymax
-    if lv1 ==  True :
+    if niveau ==  1 :
         pyxel.cls(0)
         pyxel.line(0,ymin,l,ymin,6)
         pyxel.line(0,ymax+20,l,ymax+20,6)
         pyxel.line(64,0,64,h,6) #limites de déplacement et couloir de sortie de la salle de classe
     pyxel.rect(x,y, 20, 20, 11)
 
-pyxel.run(update, draw_lv1) #fonction d'affichage des graphismes en fonction du niveau actuel
+pyxel.run(update, draw) #fonction d'affichage des graphismes en fonction du niveau actuel
