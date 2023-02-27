5d7600282835d72aef8719f1759a1cbc7fc7b65b
Vincent <81516479+vincent-fantino@users.noreply.github.com>
2023-02-27 09:49:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -141,3 +141,79 @@ def draw():  #
     
 
 pyxel.run(update, draw)
+
+
+---------------------------------------------------------------------------------------------
+
+
+def draw():  #
+    global x, y, niveau, hmin, hmax, trentièmes, secondes, minutes, casier, m
+    pyxel.cls(0)
+    #chronomètre
+    if minutes >= 0 :
+        pyxel.text(150, 32, "Timer :"+ str(minutes)+":"+ str(secondes), 8)
+    else : 
+        pyxel.cls(0)
+        pyxel.text(60*16/2, 20*16/2,"GAME OVER :(", 8)
+    
+    #Personnage
+        pyxel.rect(x,y, 20, 20, 11)
+        pyxel.rect(x+19,y, 1, 1, 8) #un pixel a chaque angle pour faciliter les collisions
+        pyxel.rect(x+19,y+19, 1, 1, 8)
+        pyxel.rect(x,y+19, 1, 1, 8)
+        
+    if niveau == 1:
+        
+        if 64*8-2 <= x <= 64*8+2: #Piege quand il passe a 64*8 il y a  30% de chance de perdre
+            m = random.randint(1,3)
+            if m == 3: #choisit un nombre entre 1 et 3, si il est égal à 3, éxecute cette boucle, il y a donc 1/3 de chances qu'elle soit éxécutée
+                pyxel.rect(5,5,50,50,5)
+        
+    #Limites de déplacement et couloir de sortie de la salle de classe
+        pyxel.line(0,hmin,l,hmin,6)
+        pyxel.line(0,hmax+20,l,hmax+20,6)
+        pyxel.line(64,0,64,h,6)
+        pyxel.line(64*8,0,64*8,h,6) 
+        
+        #obstacle niveau 1
+        pyxel.rect(20 * 6, hmax - 20, 16, 16, 6)
+        pyxel.rect(20 * 7, hmax, 16, 16, 6)
+        pyxel.rect(20 * 7, hmax - 1 * 20, 16, 16, 9)
+        pyxel.rect(20 * 7, hmax - 2 * 20, 16, 16, 9)
+        pyxel.rect(20 * 8, hmax - 1 * 20, 16, 16, 9)
+        pyxel.rect(20 * 9, hmax - 3 * 20, 16, 16, 9)
+        pyxel.rect(20 * 9, hmax - 1 * 20, 16, 16, 9)
+        pyxel.rect(20 * 10, hmax - 0 * 20, 16, 16, 9)
+        pyxel.rect(20 * 11, hmax - 2 * 20, 16, 16, 9)
+        pyxel.rect(20 * 11, hmax - 3 * 20, 16, 16, 9)
+        pyxel.rect(20 * 11, hmax - 0 * 20, 16, 16, 9)
+        pyxel.rect(20 * 12, hmax - 0 * 20, 16, 16, 9)
+        pyxel.rect(20 * 13, hmax - 2 * 20, 16, 16, 9)
+        pyxel.rect(20 * 14, hmax - 2 * 20, 16, 16, 9)
+        pyxel.rect(20 * 14, hmax - 1 * 20, 16, 16, 9)
+        pyxel.rect(20 * 15, hmax - 3 * 20, 16, 16, 9)
+        pyxel.rect(20 * 16, hmax - 0 * 20, 16, 16, 9)
+        pyxel.rect(20 * 16, hmax - 1 * 20, 16, 16, 9)
+        pyxel.rect(20 * 17, hmax - 3 * 20, 16, 16, 9)
+        pyxel.rect(20 * 17, hmax - 1 * 20, 16, 16, 9)
+        pyxel.rect(20 * 18, hmax - 0 * 20, 16, 16, 9)
+        pyxel.rect(20 * 19, hmax - 1 * 20, 16, 16, 9)
+        pyxel.rect(20 * 19, hmax - 2 * 20, 16, 16, 9)
+        pyxel.rect(20 * 19, hmax - 1 * 20, 16, 16, 9)
+        
+    elif niveau == 2:
+        pyxel.line(0, hmin, l, hmin, 7)
+        pyxel.line(0, hmax + 20, l, hmax + 20, 7)
+        
+        pyxel.rect(20 * 6, hmax - 20, 16, 16, 6)
+        
+        
+    elif niveau == 3:
+            pyxel.line(0, hmin, l, hmin, 10)
+            pyxel.line(0, hmax + 20, l, hmax + 20, 10)
+                
+        
+    pyxel.rect(x, y, 20, 20, 11)
+
+
+pyxel.run(update, draw)
