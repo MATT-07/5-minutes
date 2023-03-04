e0b4fe8518e75e98f40db14b089612d911b7948c
MATT-07 <117632549+MATT-07@users.noreply.github.com>
2023-03-04 17:50:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ niveau = 1  # variable pour savoir dans quel niveau on se trouve
 arrive = False
 trentièmes = 30
 secondes = 59
-minutes = 1
+minutes = 4
 casier = 0
 m = 0
+
 
 def deplacement(x, y):
     if pyxel.btn(pyxel.KEY_RIGHT):
@@ -57,14 +58,16 @@ def arriver(wcx1, wcx2, wcy1, wcy2, jx, jy, niveau):
     if jx > wcx1 and jx < wcx2 and jy > wcy1 and jy < wcy2:
         return True
 
+
 def update():
     global x, y, hmin, hmax, niveau, l, arrive, trentièmes, secondes, minutes
-    x, y = deplacement(x, y)  # fonction de mise à jour des coordonnées du personnage en fonction des touches appuyées, à l'aide de la fonction deplacement
+    x, y = deplacement(x,
+                       y)  # fonction de mise à jour des coordonnées du personnage en fonction des touches appuyées, à l'aide de la fonction deplacement
     arrive = arriver(l - 22, l - 18, hmin, hmax + 20, x, y, niveau)
     if arrive == True:
-        niveau+=1
-        x,y = 22, 180
-    #chronomètre
+        niveau += 1
+        x, y = 22, 180
+    # chronomètre
     trentièmes -= 1
     if trentièmes == 0:
         secondes -= 1
@@ -73,72 +76,75 @@ def update():
     if secondes == 0:
         minutes -= 1
         secondes = 59
-        
-    if 64*8-2 <= x <= 64*8+2: #Piege quand il passe a 64*8 il y a  30% de chance de perdre
-        m = random.randint(1,5)
-        if m == 3: #choisit un nombre entre 1 et 3, si il est égal à 3, éxecute cette boucle, il y a donc 1/3 de chances qu'elle soit éxécutée
-            x,y = 22,22
+
+    if 64 * 8 - 2 <= x <= 64 * 8 + 2:  # Piege quand il passe a 64*8 il y a  30% de chance de perdre
+        m = random.randint(1, 5)
+        if m == 3:  # choisit un nombre entre 1 et 3, si il est égal à 3, éxecute cette boucle, il y a donc 1/3 de chances qu'elle soit éxécutée
+            x, y = 22, 22
+
 
 def draw():  #
     global x, y, niveau, hmin, hmax, trentièmes, secondes, minutes, casier, m, niveau
     pyxel.cls(0)
-    #chronomètre
-    if minutes >= 0 :
-        pyxel.text(150, 32, "Timer : "+ str(minutes)+":"+ str(secondes), 8)
-    else : 
-        pyxel.cls(0)
-        pyxel.text(60*16/2, 20*16/2,"GAME OVER :(", 8)
-    
-    #affichage nb niveau
-    pyxel.text(5,250,"Niveau :"+str(niveau), 4)
-    
-    #Personnage
-    pyxel.rect(x,y, 20, 20, 11)
-    pyxel.rect(x+19,y, 1, 1, 8) #un pixel a chaque angle pour faciliter les collisions
-    pyxel.rect(x+19,y+19, 1, 1, 8)
-    pyxel.rect(x,y+19, 1, 1, 8)
-        
-    if niveau == 1:
-        
-    #Limites de déplacement et couloir de sortie de la salle de classe
-        pyxel.line(0,hmin,l,hmin,6)
-        pyxel.line(0,hmax+20,l,hmax+20,6)
-        pyxel.line(64,0,64,h,6)
-        pyxel.line(64*8,0,64*8,h,6) 
-        
-        #obstacle niveau 1
-        pyxel.rect(20 * 6, hmax - 20, 16, 16, 9)
-        pyxel.rect(20 * 7, hmax, 16, 16, 9)
-        pyxel.rect(20 * 7, hmax - 1 * 20, 16, 16, 9)
-        pyxel.rect(20 * 7, hmax - 2 * 20, 16, 16, 9)
-        pyxel.rect(20 * 8, hmax - 1 * 20, 16, 16, 9)
-        pyxel.rect(20 * 9, hmax - 3 * 20, 16, 16, 9)
-        pyxel.rect(20 * 9, hmax - 1 * 20, 16, 16, 9)
-        pyxel.rect(20 * 10, hmax - 0 * 20, 16, 16, 9)
-        pyxel.rect(20 * 11, hmax - 2 * 20, 16, 16, 9)
-        pyxel.rect(20 * 11, hmax - 3 * 20, 16, 16, 9)
-        pyxel.rect(20 * 11, hmax - 0 * 20, 16, 16, 9)
-        pyxel.rect(20 * 12, hmax - 0 * 20, 16, 16, 9)
-        pyxel.rect(20 * 13, hmax - 2 * 20, 16, 16, 9)
-        pyxel.rect(20 * 14, hmax - 2 * 20, 16, 16, 9)
-        pyxel.rect(20 * 14, hmax - 1 * 20, 16, 16, 9)
-        pyxel.rect(20 * 15, hmax - 3 * 20, 16, 16, 9)
-        pyxel.rect(20 * 16, hmax - 0 * 20, 16, 16, 9)
-        pyxel.rect(20 * 16, hmax - 1 * 20, 16, 16, 9)
-        pyxel.rect(20 * 17, hmax - 3 * 20, 16, 16, 9)
-        pyxel.rect(20 * 17, hmax - 1 * 20, 16, 16, 9)
-        pyxel.rect(20 * 18, hmax - 0 * 20, 16, 16, 9)
-        pyxel.rect(20 * 19, hmax - 1 * 20, 16, 16, 9)
-        pyxel.rect(20 * 19, hmax - 2 * 20, 16, 16, 9)
-        pyxel.rect(20 * 19, hmax - 1 * 20, 16, 16, 9)
-    elif niveau == 2:
-        pyxel.line(0, hmin, l, hmin, 7)
-        pyxel.line(0, hmax + 20, l, hmax + 20, 7)
-    elif niveau == 3:
+    # chronomètre
+    if minutes >= 0:
+        pyxel.text(150, 32, "Timer : " + str(minutes) + ":" + str(secondes), 8)
+        # affichage nb niveau
+        pyxel.text(5, 250, "Niveau :" + str(niveau), 4)
+
+        # Personnage
+        pyxel.rect(x, y, 20, 20, 11)
+        pyxel.rect(x + 19, y, 1, 1, 8)  # un pixel a chaque angle pour faciliter les collisions
+        pyxel.rect(x + 19, y + 19, 1, 1, 8)
+        pyxel.rect(x, y + 19, 1, 1, 8)
+
+        if niveau == 1:
+
+            # Limites de déplacement et couloir de sortie de la salle de classe
+            pyxel.line(0, hmin, l, hmin, 6)
+            pyxel.line(0, hmax + 20, l, hmax + 20, 6)
+            pyxel.line(64, 0, 64, h, 6)
+            pyxel.line(64 * 8, 0, 64 * 8, h, 6)
+
+            # obstacle niveau 1
+            pyxel.rect(20 * 6, hmax - 20, 16, 16, 9)
+            pyxel.rect(20 * 7, hmax, 16, 16, 9)
+            pyxel.rect(20 * 7, hmax - 1 * 20, 16, 16, 9)
+            pyxel.rect(20 * 7, hmax - 2 * 20, 16, 16, 9)
+            pyxel.rect(20 * 8, hmax - 1 * 20, 16, 16, 9)
+            pyxel.rect(20 * 9, hmax - 3 * 20, 16, 16, 9)
+            pyxel.rect(20 * 9, hmax - 1 * 20, 16, 16, 9)
+            pyxel.rect(20 * 10, hmax - 0 * 20, 16, 16, 9)
+            pyxel.rect(20 * 11, hmax - 2 * 20, 16, 16, 9)
+            pyxel.rect(20 * 11, hmax - 3 * 20, 16, 16, 9)
+            pyxel.rect(20 * 11, hmax - 0 * 20, 16, 16, 9)
+            pyxel.rect(20 * 12, hmax - 0 * 20, 16, 16, 9)
+            pyxel.rect(20 * 13, hmax - 2 * 20, 16, 16, 9)
+            pyxel.rect(20 * 14, hmax - 2 * 20, 16, 16, 9)
+            pyxel.rect(20 * 14, hmax - 1 * 20, 16, 16, 9)
+            pyxel.rect(20 * 15, hmax - 3 * 20, 16, 16, 9)
+            pyxel.rect(20 * 16, hmax - 0 * 20, 16, 16, 9)
+            pyxel.rect(20 * 16, hmax - 1 * 20, 16, 16, 9)
+            pyxel.rect(20 * 17, hmax - 3 * 20, 16, 16, 9)
+            pyxel.rect(20 * 17, hmax - 1 * 20, 16, 16, 9)
+            pyxel.rect(20 * 18, hmax - 0 * 20, 16, 16, 9)
+            pyxel.rect(20 * 19, hmax - 1 * 20, 16, 16, 9)
+            pyxel.rect(20 * 19, hmax - 2 * 20, 16, 16, 9)
+            pyxel.rect(20 * 19, hmax - 1 * 20, 16, 16, 9)
+        elif niveau == 2:
+            pyxel.line(0, hmin, l, hmin, 7)
+            pyxel.line(0, hmax + 20, l, hmax + 20, 7)
+        elif niveau == 3:
             pyxel.line(0, hmin, l, hmin, 10)
             pyxel.line(0, hmax + 20, l, hmax + 20, 10)
-                
-    
+
+
+    else:
+        pyxel.cls(0)
+        pyxel.text(60 * 16 / 2, 20 * 16 / 2, "GAME OVER :(", 8)
+
+
+
 
 pyxel.run(update, draw)
 
